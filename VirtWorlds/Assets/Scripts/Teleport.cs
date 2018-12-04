fbbe67af2092ace0ea89d8804a4b38a828d0aa8e
mro5772 <mro5772@IGM2025-14>
2018-12-03 20:11:59 -0500

    fdsfds

---
+++ VirtWorlds/Assets/Scripts/Teleport.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+   public GameObject Canvas;
+    public GameObject teleportSpawn;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       if(Input.GetKeyDown(KeyCode.O))
+        {
+            Canvas.SetActive(true);
+        }
+		
+	}
+
+    public void toHell()
+    {
+        Canvas.SetActive(false);
+        gameObject.transform.position = teleportSpawn.transform.position;
+    }
+}
